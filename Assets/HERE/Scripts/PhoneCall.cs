a58883e93613c9eea8dc4a7ca38c8201f6abd7c2
KiaraDominguez <kiara.dominguez1997@gmail.com>
2024-09-29 18:52:24 +0200

    script animation PHONE

---
+++ Assets/HERE/Scripts/PhoneCall.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PhoneCall : MonoBehaviour
+{
+    private Animator animator;
+    public bool calling;
+    public bool callDone;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (calling) 
+        {
+            animator.SetBool("calling",true);
+            calling = false;
+        }
+        if (callDone) 
+        {
+            animator.SetBool("calling", false);
+            callDone = true;
+        }
+    }
+}
