c16ecf94035fb1b7910c93990c091f6604e25c9a
KiaraDominguez <kiara.dominguez1997@gmail.com>
2024-10-05 15:34:29 +0200

    Ajout Systeme de dialogue

---
+++ Assets/HERE/Scripts/SpacebarButtonController.cs
@@ -0,0 +1,43 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SpacebarButtonController : MonoBehaviour
+{
+    public Button myButton;
+    public DialogueManager dialogueManager;
+
+    void Start()
+    {
+        // Optionnel : Assigner une action par défaut au bouton (ou via l'inspecteur)
+        //if (myButton != null)
+        //{
+            //myButton.onClick.AddListener(OnButtonClicked);
+        //}
+    }
+
+    void Update()
+    {
+        if (dialogueManager.isDialogueActive)
+        {
+            Debug.Log("Ok");
+    // Détecter si la barre d'espace est pressée
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Vérifier si le bouton est assigné et l'activer
+                if (myButton != null)
+                {
+                    myButton.onClick.Invoke();  // Appeler le OnClick manuellement
+                }
+            }
+        }
+        
+    }
+
+    // La méthode appelée lorsque le bouton est cliqué ou que la barre d'espace est pressée
+    void OnButtonClicked()
+    {
+        Debug.Log("Bouton activé par la barre d'espace ou un clic !");
+        // Ajouter votre logique ici, par exemple, changer de scène ou lancer une action.
+    }
+}
+
