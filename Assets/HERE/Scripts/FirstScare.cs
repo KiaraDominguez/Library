fab08db8cc2ca09836e1817f4baeaa1962c0cd5e
KiaraDominguez <kiara.dominguez1997@gmail.com>
2024-12-14 18:33:44 +0100

    firstscare + BW shader

--- Assets/HERE/Scripts/FirstScare.cs
+++ Assets/HERE/Scripts/FirstScare.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class FirstScare : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Transform player;
+    public float activationDistance = 5f;
+
+    public GameObject face;
+    public GameObject body;
+
+    private Color faceOriginalColor;
+    private Color bodyOriginalColor;
+
+    private SkinnedMeshRenderer faceSkinnedMeshRenderer;
+    private SkinnedMeshRenderer bodySkinnedMeshRenderer;
+
+
     void Start()
     {
-        
+        faceSkinnedMeshRenderer = face.GetComponent<SkinnedMeshRenderer>();
+        faceOriginalColor = faceSkinnedMeshRenderer.material.color;
+
+        bodySkinnedMeshRenderer = body.GetComponent<SkinnedMeshRenderer>();
+        bodyOriginalColor = bodySkinnedMeshRenderer.material.color;
+
+        SetObjectVisibility(false);        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calculer la distance entre le joueur et l'objet
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        // Vérifier si la distance est inférieure ou égale à la distance d'activation
+        if (distance <= activationDistance)
+        {
+            SetObjectVisibility(true);
+        }
+        else
+        {
+            SetObjectVisibility(false);
+        }
+        // mettre coroutine qui setVisible true avec un timer ...s quand le joueur est a la bonne distance avec un bool en condiction => ne peut qu'arriver 1 seule fois
+    }
+
+    private void SetObjectVisibility(bool isVisible)
+    {
+        if (isVisible)
+        {
+            faceSkinnedMeshRenderer.material.color = faceOriginalColor;
+            bodySkinnedMeshRenderer.material.color = bodyOriginalColor;
+        }
+        else
+        {
+           faceSkinnedMeshRenderer.material.color = new Color(faceOriginalColor.r, faceOriginalColor.g, faceOriginalColor.b, 0f); // Rendre transparent
+           bodySkinnedMeshRenderer.material.color = new Color(bodyOriginalColor.r, bodyOriginalColor.g, bodyOriginalColor.b, 0f); // Rendre transparent
+        }
     }
 }
